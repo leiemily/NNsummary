55aa19a117579404b24165b7dfeaddf64af9f0ba
leiemily <93039907+leiemily@users.noreply.github.com>
2025-06-04 15:49:09 +0800

    Delete 4simu code--signal tests directory

--- 4simu code--signal tests/model I/gradient2_s.m
+++
@@ -1,8 +0,0 @@
-function df2 = gradient2_s(f,h)
-df2 = zeros(1,length(f));
-for j = 2:(length(f)-1) 
-df2(j) = (f(j-1) - 2*f(j) + f(j+1))/h^2; %三点中心差分
-end
-df2(1) = (f(1) - 2*f(2) + f(3))/h^2;%三点前向差分
-df2(end) = (f(end-2) - 2*f(end-1) + f(end))/h^2;%三点后向差分
-end
--- 4simu code--signal tests/model III/linearcom_s.m
+++
@@ -1,6 +0,0 @@
-function ss = linearcom_s(ak,TT,tt,Numberof_Tp)
-  ss = zeros(1,length(tt));
-  for k = 1:length(ak)
-      ss = ss + 1/Numberof_Tp*ak(k)*(1-cos(2*pi*tt/TT(k)));
-  end
-end
