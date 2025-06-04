38478d81abe2ae71543ce76b478584ca62a8b547
leiemily <93039907+leiemily@users.noreply.github.com>
2025-06-04 15:47:49 +0800

    Delete 1simu code--ecoli performance directory

--- 1simu code--ecoli performance/model II/linearexpcom_s.m
+++
@@ -1,9 +0,0 @@
-function ss = linearexpcom_s(ak,TT,tt,step_maxvalue,per)
-  ss = zeros(1,length(tt));
-  for k = 1:length(ak)
-      ss = ss + ak(k)*(1-cos(2*pi*tt/TT(k)));
-  end
-%   ss = exp(ss - (abs(log(step_maxvalue)) + step_maxvalue));
-  ss = exp(ss - (max(ss)-abs(log((1-per)*step_maxvalue))));
-%   ss = exp(ss - (step_maxvalue-abs(log(step_maxvalue))));
-end
